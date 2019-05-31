b13432bad68d4133bbd6b363516a4d2566526566
realVato <44781061+realVato@users.noreply.github.com>
2019-05-31 03:24:29 +0300

    Transition Scroll added for navbar & video-text

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Smooth Scroll
 $('.transition-btn').on('click', function(event) {
     if (this.hash !== '') {
       event.preventDefault();
@@ -15,3 +16,20 @@ $('.transition-btn').on('click', function(event) {
       );
     }
   });
+
+// Transition on scroll
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.navbar').css({'background' : '#000', 'height' : '40px', 'border-bottom' : '0'});
+    } else {
+      $('.navbar').css({'background' : 'rgba(3, 3, 3, 0.01)', 'height' : '90px', 'border-bottom' : '1px solid rgb(75, 75, 75)'});
+    }
+
+    if ($(this).scrollTop() > 250) {
+      $('.video-text').css({'opacity' : '0'});
+    } else {
+      $('.video-text').css({'opacity' : '1'});
+    }
+  });
+});
